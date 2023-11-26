db749d90eb03bcd9675a9dd923d028d600482738
Mark Semeniuk <99902956+Mar4ik10@users.noreply.github.com>
2023-11-26 22:10:29 +0100

    SEO: add robots.txt

--- theatreproj/theatrehome/urls.py
+++ theatreproj/theatrehome/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path("abouttheatre/", views.abouttheatre, name="abouttheatre"),
     path("selectTickets/<slug:show_id>/", views.selectTickets, name="selectTickets"),
     path("afisha/", views.afisha, name='afisha'),
+    re_path(r'^robots\.txt$', TemplateView.as_view(template_name="theatreproj/robots.txt", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
